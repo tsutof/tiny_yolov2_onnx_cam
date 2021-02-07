bbea501d45878926633024f6db6ec49dede3a918
Tsutomu Furuse <47056802+tsutof@users.noreply.github.com>
2021-02-07 20:28:08 +0900

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -3,4 +3,4 @@
 cp /etc/apt/sources.list.d/nvidia-l4t-apt-source.list .
 cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc .
 
-sudo docker build --no-cache -t tiny_yolov2_onnx_cam:jetpack4.5 .
+sudo docker build --no-cache -t tiny_yolov2_onnx_cam:l4t-r32.5.0 .
