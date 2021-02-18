7c1caeb7aeb87a356a3bf427d69e0606c744e8ae
Tsutomu Furuse <47056802+tsutof@users.noreply.github.com>
2021-02-18 21:30:52 +0900

    Delete docker_build.sh

--- docker_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp /etc/apt/sources.list.d/nvidia-l4t-apt-source.list .
-cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc .
-
-sudo docker build --no-cache -t tiny_yolov2_onnx_cam:l4t-r32.5.0 .
