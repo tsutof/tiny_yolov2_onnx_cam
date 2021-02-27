94600234b6e5629bfbe7ab8af57ef211addb23a4
Tsutomu Furuse <47056802+tsutof@users.noreply.github.com>
2021-02-27 20:36:31 +0900

    Add files via upload

--- scripts/compose-down.sh
+++ scripts/compose-down.sh
@@ -5,5 +5,6 @@ set -eu
 source scripts/docker_base.sh
 
 export BASE_IMAGE=${BASE_IMAGE}
+export L4T_VERSION=${L4T_VERSION}
 
 sudo -E ~/.local/bin/docker-compose down
--- scripts/compose-up.sh
+++ scripts/compose-up.sh
@@ -8,5 +8,6 @@ cp /etc/apt/sources.list.d/nvidia-l4t-apt-source.list .
 cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc .
 
 export BASE_IMAGE=${BASE_IMAGE}
+export L4T_VERSION=${L4T_VERSION}
 
 sudo -E ~/.local/bin/docker-compose up
