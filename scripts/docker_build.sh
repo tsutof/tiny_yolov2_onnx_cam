c996e07a94dc11ffa83648412e46132d8cf22b86
Tsutomu Furuse <47056802+tsutof@users.noreply.github.com>
2021-02-28 16:56:41 +0900

    Removed --no-cache option from docker build

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -7,4 +7,4 @@ source scripts/docker_base.sh
 cp /etc/apt/sources.list.d/nvidia-l4t-apt-source.list .
 cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc .
 
-sudo docker build --no-cache --build-arg BASE_IMAGE=${BASE_IMAGE} -t tiny_yolov2_onnx_cam:l4t-r${L4T_VERSION} .
+sudo docker build --build-arg BASE_IMAGE=${BASE_IMAGE} -t tiny_yolov2_onnx_cam:l4t-r${L4T_VERSION} .
