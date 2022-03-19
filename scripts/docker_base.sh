15dd8168109e18378ad64af6002745462a5f9c72
tsutof <47056802+tsutof@users.noreply.github.com>
2022-03-19 09:24:56 +0900

    Copied from dusty-nv/jetson-containers

--- scripts/docker_base.sh
+++ scripts/docker_base.sh
@@ -4,7 +4,7 @@ source scripts/l4t_version.sh
 
 BASE_IMAGE="nvcr.io/nvidia/l4t-base:r$L4T_VERSION"
 #BASE_DEVEL="nvcr.io/nvidian/nvidia-l4t-base:r$L4T_VERSION"
-BASE_DEVEL=$BASE_IMAGE
+BASE_DEVEL="nvcr.io/nvidia/nvidia-l4t-base:r$L4T_VERSION"
 
 if [ $L4T_RELEASE -eq 32 ]; then
 	if [ $L4T_REVISION_MAJOR -eq 4 ]; then
@@ -14,10 +14,12 @@ if [ $L4T_RELEASE -eq 32 ]; then
 	elif [ $L4T_REVISION_MAJOR -eq 5 ]; then
 		if [ $L4T_REVISION_MINOR -eq 1 ]; then
 			BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.5.0"
-		elif [ $L4T_REVISION_MINOR -gt 1 ]; then
+		elif [ $L4T_REVISION_MINOR -eq 2 ]; then
+			BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.5.0"
+		elif [ $L4T_REVISION_MINOR -gt 2 ]; then
 			BASE_IMAGE=$BASE_DEVEL
 		fi
-	elif [ $L4T_REVISION_MAJOR -gt 5 ]; then
+	elif [ $L4T_REVISION_MAJOR -gt 6 ]; then
 		BASE_IMAGE=$BASE_DEVEL
 	fi
 fi
