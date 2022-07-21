14f0223f467a2a52a29fd19f3bb0bcaf6a969df7
Tsutomu Furuse <47056802+tsutof@users.noreply.github.com>
2022-07-21 21:12:50 +0900

    Modified to support L4T 32.7.2

--- scripts/docker_base.sh
+++ scripts/docker_base.sh
@@ -3,24 +3,20 @@
 source scripts/l4t_version.sh
 
 BASE_IMAGE="nvcr.io/nvidia/l4t-base:r$L4T_VERSION"
-#BASE_DEVEL="nvcr.io/nvidian/nvidia-l4t-base:r$L4T_VERSION"
 BASE_DEVEL="nvcr.io/nvidia/l4t-base:r$L4T_VERSION"
 
+
 if [ $L4T_RELEASE -eq 32 ]; then
-	if [ $L4T_REVISION_MAJOR -eq 4 ]; then
-		if [ $L4T_REVISION_MINOR -gt 4 ]; then
-			BASE_IMAGE=$BASE_DEVEL
-		fi
-	elif [ $L4T_REVISION_MAJOR -eq 5 ]; then
+	if [ $L4T_REVISION_MAJOR -eq 5 ]; then
 		if [ $L4T_REVISION_MINOR -eq 1 ]; then
 			BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.5.0"
 		elif [ $L4T_REVISION_MINOR -eq 2 ]; then
 			BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.5.0"
-		elif [ $L4T_REVISION_MINOR -gt 2 ]; then
-			BASE_IMAGE=$BASE_DEVEL
 		fi
-	elif [ $L4T_REVISION_MAJOR -gt 7 ]; then
-		BASE_IMAGE=$BASE_DEVEL
+    elif [ $L4T_REVISION_MAJOR -eq 7 ]; then
+        if [ $L4T_REVISION_MINOR -eq 2 ]; then
+            BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.7.1"
+        fi
 	fi
 fi
 	
